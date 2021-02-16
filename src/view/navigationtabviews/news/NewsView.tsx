b4de10d07770cf49b2d63f6afc5827aa24e7b4bb
eman1970 <tommy.hallqvist@gmail.com>
2021-02-16 06:33:22 +0100

    Inputfield testUser

--- src/view/navigationtabviews/news/NewsView.tsx
+++ src/view/navigationtabviews/news/NewsView.tsx
@@ -1,32 +1,48 @@
 import React from 'react'
 import { useContext, useState } from "react";
 import './NewsView.css'
- 
+
 import { TestContext } from '../../../shared/provider/TestProvider';
-
-
 
 
 export const NewsView = () => {
     const [testUser, setTestUser] = useContext(TestContext)
-  
+    const [inputValue, setInputValue] = useState<any>()
+
+    const updateGlobalList = (x: any) => {
+      return  <div dangerouslySetInnerHTML={{__html: '<br />'}} /> + setTestUser(testUser.concat(x)) 
+    }
+
+    const handleSubmit = () =>{
+       
+       
+        // clearing the values
+        setTestUser("");   
+    
+    
+    }
 
  
-
+ 
+       
 
     return (
         <div>
 
             <h1>This is the newsView</h1>
             <p>{testUser}</p>
-            <input className="testForm" placeholder="update and display in BrandsView" type="text" onChange={event => setTestUser(event.target.value)} />
-       
         
-
+            <input className="testForm" placeholder="update testUser (also on brandsView)" onChange={event => setInputValue(event.target.value)} />
+            <button onClick={() => updateGlobalList(inputValue)}>Add to list</button>
+            <button onClick={() => console.log(testUser)}>Console</button>
+            <button onClick={() => handleSubmit()}>Clear</button>
+        
+       
+            
         </div>
 
     )
 
 }
 
-export default  NewsView
+export default NewsView
